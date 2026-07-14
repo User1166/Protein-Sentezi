a10c92fbcff43af7be8879952b874f9dea228be7
Ehckr <hesapolsun150@gmail.com>
2026-07-14 17:02:47 +0300

    correct versiyon

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    """Flask uygulaması için temel konfigürasyon"""
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///protein_app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'dev-secret-key-change-in-production'
-    JSON_AS_ASCII = False
-    JSON_SORT_KEYS = False
